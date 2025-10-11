89d0cf416d5c98b410084f12099b71f3999c2175
Luces Umbrarum <87826179+Xpycode@users.noreply.github.com>
2025-10-11 20:45:31 +0200

    fix: Define all custom notification names
    Adds the missing .openFile and .showRawMarkdown notification names to the Notification.Name extension to resolve compiler errors.

--- ProjectProgressTracker/Extensions/Notifications.swift
+++ ProjectProgressTracker/Extensions/Notifications.swift
@@ -9,4 +9,6 @@ import Foundation
 
 extension Notification.Name {
     static let spacebarPressed = Notification.Name("spacebarPressed")
+    static let openFile = Notification.Name("openFile")
+    static let showRawMarkdown = Notification.Name("showRawMarkdown")
 }
